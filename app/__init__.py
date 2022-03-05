46660dfc9b66b3bf2798234b4d6ee60a7206d8e6
leozhang233 <leo865843040@gmail.com>
2022-03-05 15:14:11 -0800

    facial extraction

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 # $ export FLASK_ENV=development; flask run
 
 from flask import Flask, g, render_template, request, redirect, url_for
+import cv2
 import numpy as np
 import sqlite3
 import pickle
@@ -103,8 +104,28 @@ def result():
         filename = "img.jpg"
         img1 = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         # read the image
-        img = image.load_img(os.path.join(app.root_path, "static/uploads", filename), target_size=(144,144))
+        img = cv2.imread(os.path.join(app.root_path, "static/uploads", "img.jpg"))
+        # Convert into grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+  
+        # Draw rectangle around the faces and crop the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            faces = img[y:y + h, x:x + w]
+            cv2.imwrite(os.path.join(app.root_path, "static/uploads", "face.jpg"), faces)
+            
+        # detected image
+        cv2.imwrite(os.path.join(app.root_path, "static/uploads", "detected.jpg"), img)
+
+        # read the image
+        face = os.path.join(app.config['UPLOAD_FOLDER'], "face.jpg")
+        detected = os.path.join(app.config['UPLOAD_FOLDER'], "detected.jpg")
+
+        img = image.load_img(os.path.join(app.root_path, "static/uploads", "face.jpg"), target_size=(144,144))
         data  = image.img_to_array(img)
         # convert to numpy array
 
@@ -121,7 +142,7 @@ def result():
         index = np.argmax(model.predict(it), axis = 1)[0]
         label = CLASS_LABELS[index]
 
-        return render_template('result.html', image = img1, labels = label)
+        return render_template('result.html', image = face, labels = label, image1 = detected)
     else:
         try:
             if request.form['ans'] == "neg-quote":
